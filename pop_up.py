5baff52082b1145aa0c3f5041b3012edacee5f51
Joshua Imarhiagbe <px267@cummins.com>
2021-04-12 02:26:50 -0400

    Created notification handler that allows user reaction and base welcom process.

--- pop_up.py
+++ pop_up.py
@@ -17,19 +17,26 @@ layout = [[sg.Image(data=get_image_from_file("DA Care Window Logo Small.png"), b
            sg.Text('Please, tell me where it hurts', pad=((0, 10), (0, 0)), auto_size_text=True,
                    background_color=WIN_COLOR, text_color=TEXT_COLOR)]]
 
-layout += [[sg.CBox(option, auto_size_text=True, background_color=WIN_COLOR, text_color=TEXT_COLOR)]
-           for option in CONFIG.get_option_list()]  # the checkboxes and descriptions
+# Add the checkboxes and descriptions
+layout += [[sg.CBox(option, key=f"-Option-#{option}-", auto_size_text=True, background_color=WIN_COLOR,
+                    text_color=TEXT_COLOR)] for option in CONFIG.get_option_list()]
 
 # Add the buttons for the selection pop-up
 layout += [[sg.Frame('', layout=([[sg.Button(image_data=get_image_from_file("Final Yes.png"), ),
             sg.Button(image_data=get_image_from_file("Final No.png"))]]), background_color=WIN_COLOR,
-                     border_width=0).Widget.config(cursor="hand2")]]
+                     border_width=0, key="-Buttons-")]]
 
 window = sg.Window('', layout, keep_on_top=True, background_color=WIN_COLOR, grab_anywhere=True,
-                   no_titlebar=True, auto_size_text=True)
+                   no_titlebar=True, auto_size_text=True, finalize=True)
+
+# Set cursor format for the buttons and the checkboxes
+window["-Buttons-"].Widget.config(cursor="hand2")
+for option in CONFIG.get_option_list():
+    window[f"-Option-#{option}-"].Widget.config(cursor="hand2")
 
 while True:
     event, values = window.read()
+
     print(event, values)
     if event in (None, 'Exit'):
         break
