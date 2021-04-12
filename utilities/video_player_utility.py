8adf8f778aa6d6cbf3d3a4f0b00c754eae9b8467
jeyakishore <jeyakishore.sugumar@cummins.com>
2021-04-12 14:39:33 +0530

    test

--- utilities/video_player_utility.py
+++ utilities/video_player_utility.py
@@ -2,9 +2,19 @@ import pafy
 import webview
 
 
-def play_video(url):
-    video = pafy.new(url)
-    best = video.getbest()
-    webview.create_window('Video Player', best.url)
+def play_video(url, area):
+    # video = pafy.new(url)
+    # best = video.getbest()
+    video_url = "https://www.youtube.com/embed/{url}?autoplay=1&modestbranding=1&start={}".format(url)
+    if area == "Neck":
+        video_url = video_url.format("77")
+    elif area == "Shoulder":
+        video_url = video_url.format("77")
+    elif area == "Lower Body":
+        video_url = video_url.format("77")
+    elif area == "Upper Body":
+        video_url = video_url.format("77")
+    else:
+        video_url = video_url.format("77")
+    webview.create_window('Video Player', video_url)
     webview.start()
-
